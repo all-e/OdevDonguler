d448a64649cd0438fefd773928bd5391e28357b7
Muhammet Ali Özdemir <58704647+all-e@users.noreply.github.com>
2019-12-11 17:29:48 +0300

    Ödev For Döngüleri

---
+++ OdevDongulerApp/OdevDongulerApp/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdevDongulerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. For döngüsü / iki aralık arasını toplama
+
+            //Console.Write("Birinci Sayıyı giriniz: ");
+            //int a = int.Parse(Console.ReadLine());
+
+            //Console.Write("İkinci Sayıyı giriniz: ");
+            //int b = int.Parse(Console.ReadLine());
+
+            //int sonuc = 0;
+            //for (int i = a; i <= b; i++)
+            //{
+            //    Console.WriteLine(i);
+
+            //    sonuc = sonuc + i;
+            //}
+            //Console.WriteLine("Toplam:{0}", sonuc);
+            //Console.ReadKey();
+
+            // 2.For döngüsü / alfabe
+            //char i;
+            //for (i = 'a'; i <= 'z'; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadKey();
+
+            // 3.For döngüsü / Ekrana integera atanan değer kadar merhaba dünya yazdırmak
+            //int a = 10;
+            //for (int i = 1; i <= a; i++)
+            //{
+            //    Console.WriteLine(i + "- Merhaba Dünya");
+            //}
+
+            //Console.ReadKey();
+
+            // 4.For döngüsü / İstenilen metnin tekrarlanması
+
+            //Console.Write("Tekrarlanması İstenen Metni Giriniz: ");
+            //string a = Convert.ToString(Console.ReadLine());
+
+            //Console.Write("Kaç Kere Tekrarlansın?: ");
+            //int b = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= b; i++)
+            //{
+            //    Console.WriteLine(i + a);
+            //}
+
+            //Console.ReadKey();
+
+            // 5.For döngüsü / girilen sayıyın çift ve tek toplamını buluyor
+
+            //int cifttoplam = 0;
+            //int tektoplam = 0;
+            //int sayi = 0;
+            //Console.Write("Bir sayı giriniz: ");
+            //sayi = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < sayi; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        cifttoplam += i;
+            //    }
+            //    else
+            //    {
+            //        tektoplam += i;
+            //    }
+            //}
+            //Console.WriteLine(sayi + " sayısına kadar tek sayıların toplamı: " + tektoplam);
+            //Console.WriteLine(sayi + " sayısına kadar çift sayıların toplamı: " + cifttoplam);
+            //Console.ReadKey();
+
+            // 6.For döngüsü // 3 e ve 5 e tam bölünebilen
+
+            //Console.Write("Kaça kadar 3'e ve 5'e tam bölünebilen sayılar gösterilsin?: ");
+            //int b = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= b; i++)
+            //{
+            //    if (i % 3 == 0 && i % 5 == 0)
+            //    { Console.WriteLine(i); }
+            //}
+            //Console.ReadKey();
+
+            // 7.For döngüsü / yazdığın sayıdan geriye doğru yazdırır
+
+            //Console.Write("Bir sayı gir: ");
+            //int a = int.Parse(Console.ReadLine());
+            //for (int i = a; i > 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadKey();
+
+            // 8.For döngüsü / Girilen değere kadar tek olanların karesini çift olanların küpünü alır
+
+            //Console.Write("Bir sayı gir: ");
+            //int a = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= a; i++)
+            //{
+            //    if (i % 2 == 1)
+            //    {
+            //        Console.WriteLine(i + " sayısının karesi : " + (i * i));
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(i + " sayısının küpü : " + (i * i * i));
+            //    }
+            //}
+            //Console.ReadKey();
+
+            // 9.For döngüsü / Diküçgen Yazdırma
+
+            //Console.Write("Bir karakter gir: ");
+            //string kar1 = Convert.ToString(Console.ReadLine());
+            //Console.Write("Bir karakter gir: ");
+            //string kar2 = Convert.ToString(Console.ReadLine());
+            //Console.Write("Yükseklik giriniz :");
+            //int yukseklik = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < yukseklik; i++)
+            //{
+            //    Console.WriteLine(kar1);
+            //    kar1 += kar2;
+            //}
+            //Console.ReadKey();
+
+            // 10.For döngüsü / Faktöriyel hesaplama
+
+            //int i, sayi, faktoriyel = 1;
+            //Console.Write("Sayı giriniz: ");
+            //sayi = int.Parse(Console.ReadLine());
+            //for (i = 1; i <= sayi; i++)
+            //{
+            //    faktoriyel = faktoriyel * i;
+            //}
+            //Console.Write("Sonuç:" + faktoriyel);
+            //Console.ReadKey();
+            
+        }
+    }
+}
